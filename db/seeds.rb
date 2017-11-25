c3585d7d7a837ab6b524530d6b165f7e8db7073a
Yashraj Jhala <jhalayashraj@gmail.com>
2017-11-26 01:04:21 +0530

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+             email: "user@example.com",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true,
@@ -23,3 +23,11 @@ content = Faker::Lorem.sentence(5)
 users.each { |user| user.microposts.create!(content: content) }
 end
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
